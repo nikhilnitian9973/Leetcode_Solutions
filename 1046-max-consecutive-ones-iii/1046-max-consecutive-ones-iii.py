2c8a0433db3845ee28b5580acf51e33b0c94edeb
Nikhil <nikhilnitian99@gmail.com>
2026-07-20 14:40:08 +0530

    Time: 116 ms (15.54%), Space: 13.8 MB (76.87%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def longestOnes(self, nums, k):
+        
+        
+        left = 0
+        right = 0
+        maxi = 0
+        count_zero = 0
+        while right<len(nums):
+            
+            
+            if nums[right] == 0:
+                count_zero +=1
+            
+            while count_zero > k:
+                if nums[left] == 0:
+                    count_zero -=1
+                left +=1
+            
+            maxi = max(maxi,right-left+1)
+            right +=1
+        return maxi
+
+                
+
